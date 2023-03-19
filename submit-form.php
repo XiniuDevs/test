e774475cda89a56344595d68c63c5948a2703f45
Xiniu <119132375+XiniuDevs@users.noreply.github.com>
2023-03-19 17:10:49 -0400

    Add files via upload

---
+++ submit-form.php
@@ -0,0 +1,43 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form fields and trim whitespace
+  $name = trim($_POST["name"]);
+  $email = trim($_POST["email"]);
+  $message = trim($_POST["message"]);
+
+  // Check if name and message are not empty
+  if (empty($name) || empty($message)) {
+    echo "Name and message are required.";
+    exit;
+  }
+
+  // Check if email is valid
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Invalid email address.";
+    exit;
+  }
+
+  // Set the recipient email address
+  $to = "youremail@example.com";
+
+  // Set the email subject
+  $subject = "New message from $name";
+
+  // Set the email message
+  $email_message = "Name: $name\n";
+  $email_message .= "Email: $email\n\n";
+  $email_message .= "Message:\n$message";
+
+  // Set the email headers
+  $headers = "From: $email\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "X-Mailer: PHP/" . phpversion();
+
+  // Send the email
+  if (mail($to, $subject, $email_message, $headers)) {
+    echo "Thank you for your message!";
+  } else {
+    echo "Oops! Something went wrong and we couldn't send your message.";
+  }
+}
